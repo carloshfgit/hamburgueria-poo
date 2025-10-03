914b66a23fb251071ed9826c780538fe998eeef0
chcosta <carloshf377@gmail.com>
2025-10-03 10:28:32 -0300

    criando a classe Cliente

--- models/cliente.py
+++ models/cliente.py
@@ -1,1 +1,11 @@
-        
+from .endereco import Endereco
+
+class Cliente:
+
+    def __init__(self, nome: str, telefone: str, endereco: Endereco):
+        self.nome = nome
+        self._telefone = telefone
+        self._endereco = endereco
+
+    def __str__(self) -> str:
+        return f"Cliente: {self._nome}, Tel: {self._telefone}\nEndereço: {self._endereco}"
