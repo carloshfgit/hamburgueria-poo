d7562a2d651b9cfc8c797bb27d3b0d8688889fe8
chcosta <carloshf377@gmail.com>
2025-10-23 20:28:05 -0300

    finalizando comentários visibilidade do código

--- models/item_pedido.py
+++ models/item_pedido.py
@@ -1,5 +1,3 @@
-# Arquivo: models/item_pedido.py
-
 from .produto import Produto
 
 class ItemPedido:
@@ -9,7 +7,7 @@ class ItemPedido:
         self._produto = produto
         self._quantidade = quantidade
 
-    # <<< MUDANÇA: Propriedades (Getters) para consistência >>>
+    #exemplos de getters
     @property
     def produto(self) -> Produto:
         return self._produto
@@ -18,14 +16,10 @@ class ItemPedido:
     def quantidade(self) -> int:
         return self._quantidade
 
-    #transforma o metodo subtotal em um atributo virtual
+    #calcula o subtotal
     @property
     def subtotal(self) -> float:
-        # <<< MUDANÇA: Usa as propriedades de ItemPedido e (futuras) de Produto >>>
-        # Isso assume que Produto terá uma propriedade .preco
         return self.produto.preco * self.quantidade
     
     def __str__(self) -> str:
-        # <<< MUDANÇA: Usa as propriedades de ItemPedido e (futuras) de Produto >>>
-        # Isso assume que Produto terá uma propriedade .nome
         return f"Item: {self.produto.nome} | Qtd: {self.quantidade} | Subtotal: R$ {self.subtotal:.2f}"
