fc3066b0135c7b1a48deb39d23ea6f33d771cadd
chcosta <carloshf377@gmail.com>
2025-10-23 20:22:34 -0300

    comentando processador_pagamento.py

--- models/processador_pagamento.py
+++ models/processador_pagamento.py
@@ -1,22 +1,16 @@
-# Arquivo: models/processador_pagamento.py (Refatorado)
-
 from .pedido import Pedido
 
 class ProcessadorPagamento:
 
-    #simula o processamento de um pagamento, e retorna true se concluído e false se falhar
+    #simula o processamento de um pagamento
     def processar(self, pedido: Pedido, forma_pagamento: str) -> bool:
-        # Estas linhas usam a propriedade .total, o que está correto!
+
         print(f"Iniciando pagamento de R$ {pedido.total:.2f} para o pedido.")
         print(f"Forma de pagamento: {forma_pagamento}")
-        
-        # ... (aqui poderia ter uma lógica real de pagamento) ...
-        
+
         print("Pagamento processado com sucesso!")
         
-        # <<< MUDANÇA: Delegação da responsabilidade >>>
-        # Em vez de: pedido._status = "Pago"
-        # Nós "avisamos" o pedido para que ele mesmo mude seu status.
+        #muda status do pedido
         pedido.marcar_como_pago()
         
         return True
